e33d5124adf3febf5deabc5f95d8f5dbc520446f
Derek McGowan <derek@mcgstyle.net>
2014-10-22 10:49:23 -0700

    Add default TLS using engine keys
    Signed-off-by: Derek McGowan <derek@mcgstyle.net> (github: dmcgowan)

--- docker/docker.go
+++ docker/docker.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"crypto/tls"
 	"crypto/x509"
 	"fmt"
@@ -8,6 +9,7 @@ import (
 	"os"
 	"path"
 	"strings"
+	"time"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/docker/docker/api"
@@ -20,10 +22,12 @@ import (
 )
 
 const (
-	defaultTrustKeyFile = "key.json"
-	defaultCaFile       = "ca.pem"
-	defaultKeyFile      = "key.pem"
-	defaultCertFile     = "cert.pem"
+	defaultTrustKeyFile   = "key.json"
+	defaultHostKeysFile   = "allowed_hosts.json"
+	defaultClientKeysFile = "authorized_keys.json"
+	defaultCaFile         = "ca.pem"
+	defaultKeyFile        = "key.pem"
+	defaultCertFile       = "cert.pem"
 )
 
 func main() {
@@ -68,6 +72,7 @@ func main() {
 		}
 		flHosts = append(flHosts, defaultHost)
 	}
+	*flTlsVerify = true
 
 	if *flDaemon {
 		mainDaemon()
@@ -100,7 +105,12 @@ func main() {
 		cli       *client.DockerCli
 		tlsConfig tls.Config
 	)
-	tlsConfig.InsecureSkipVerify = true
+
+	// Load known hosts
+	knownHosts, err := libtrust.LoadKeySetFile(*flTrustHosts)
+	if err != nil {
+		log.Fatalf("Could not load trusted hosts file: %s", err)
+	}
 
 	// Regardless of whether the user sets it to true or false, if they
 	// specify --tlsverify at all then we need to turn on tls
@@ -110,36 +120,114 @@ func main() {
 
 	// If we should verify the server, we need to load a trusted ca
 	if *flTlsVerify {
-		certPool := x509.NewCertPool()
-		file, err := ioutil.ReadFile(*flCa)
+		allowedHosts, err := libtrust.FilterByHosts(knownHosts, protoAddrParts[1], false)
 		if err != nil {
-			log.Fatalf("Couldn't read ca cert %s: %s", *flCa, err)
+			log.Fatalf("Error filtering hosts: %s", err)
 		}
-		certPool.AppendCertsFromPEM(file)
+		certPool, err := libtrust.GenerateCACertPool(trustKey, allowedHosts)
+		if err != nil {
+			log.Fatalf("Could not create CA pool: %s", err)
+		}
+		if *flCa != "" {
+			file, err := ioutil.ReadFile(*flCa)
+			if err != nil {
+				if !os.IsNotExist(err) {
+					log.Fatalf("Couldn't read ca cert %s: %s", *flCa, err)
+				} else {
+					tlsConfig.ServerName = "docker"
+				}
+			} else {
+				certPool.AppendCertsFromPEM(file)
+			}
+		} else {
+			tlsConfig.ServerName = "docker"
+		}
 		tlsConfig.RootCAs = certPool
 		tlsConfig.InsecureSkipVerify = false
 	}
 
 	// If tls is enabled, try to load and send client certificates
 	if *flTls || *flTlsVerify {
-		_, errCert := os.Stat(*flCert)
-		_, errKey := os.Stat(*flKey)
-		if errCert == nil && errKey == nil {
-			*flTls = true
-			cert, err := tls.LoadX509KeyPair(*flCert, *flKey)
-			if err != nil {
+		*flTls = true
+		cert, err := tls.LoadX509KeyPair(*flCert, *flKey)
+		if err != nil {
+			if !os.IsNotExist(err) {
 				log.Fatalf("Couldn't load X509 key pair: %s. Key encrypted?", err)
+			} else {
+				x509Cert, certErr := libtrust.GenerateSelfSignedClientCert(trustKey)
+				if certErr != nil {
+					log.Fatalf("Certificate generation error: %s", certErr)
+				}
+				cert = tls.Certificate{
+					Certificate: [][]byte{x509Cert.Raw},
+					PrivateKey:  trustKey.CryptoPrivateKey(),
+					Leaf:        x509Cert,
+				}
 			}
-			tlsConfig.Certificates = []tls.Certificate{cert}
 		}
+		tlsConfig.Certificates = []tls.Certificate{cert}
+	}
+
+	if protoAddrParts[0] != "unix" && (!*flInsecure) {
+		savedInsecure := tlsConfig.InsecureSkipVerify
+		tlsConfig.InsecureSkipVerify = true
+		testConn, connErr := tls.Dial(protoAddrParts[0], protoAddrParts[1], &tlsConfig)
+		if connErr != nil {
+			log.Fatalf("TLS Handshake error: %s", connErr)
+		}
+		opts := x509.VerifyOptions{
+			Roots:         tlsConfig.RootCAs,
+			CurrentTime:   time.Now(),
+			DNSName:       tlsConfig.ServerName,
+			Intermediates: x509.NewCertPool(),
+		}
+
+		certs := testConn.ConnectionState().PeerCertificates
+		for i, cert := range certs {
+			if i == 0 {
+				continue
+			}
+			opts.Intermediates.AddCert(cert)
+		}
+		_, err = certs[0].Verify(opts)
+		if err != nil {
+			if _, ok := err.(x509.UnknownAuthorityError); ok {
+				pubKey, err := libtrust.FromCryptoPublicKey(certs[0].PublicKey)
+				if err != nil {
+					log.Fatalf("Error extracting public key from certificate: %s", err)
+				}
+
+				if promptUnknownKey(pubKey, protoAddrParts[1]) {
+					pubKey.AddExtendedField("hosts", []string{protoAddrParts[1]})
+					err = libtrust.AddKeySetFile(*flTrustHosts, pubKey)
+					if err != nil {
+						log.Fatalf("Error saving updated host keys file: %s", err)
+					}
+
+					ca, err := libtrust.GenerateCACert(trustKey, pubKey)
+					if err != nil {
+						log.Fatalf("Error generating CA: %s", err)
+					}
+					tlsConfig.RootCAs.AddCert(ca)
+				} else {
+					log.Fatalf("Cancelling request due to invalid certificate")
+				}
+			} else {
+				log.Fatalf("TLS verification error: %s", connErr)
+			}
+		}
+
+		testConn.Close()
+		tlsConfig.InsecureSkipVerify = savedInsecure
+
 		// Avoid fallback to SSL protocols < TLS1.0
 		tlsConfig.MinVersion = tls.VersionTLS10
 	}
 
-	if *flTls || *flTlsVerify {
-		cli = client.NewDockerCli(os.Stdin, os.Stdout, os.Stderr, trustKey, protoAddrParts[0], protoAddrParts[1], &tlsConfig)
-	} else {
+	if protoAddrParts[0] == "unix" || *flInsecure {
 		cli = client.NewDockerCli(os.Stdin, os.Stdout, os.Stderr, trustKey, protoAddrParts[0], protoAddrParts[1], nil)
+	} else {
+		cli = client.NewDockerCli(os.Stdin, os.Stdout, os.Stderr, trustKey, protoAddrParts[0], protoAddrParts[1], &tlsConfig)
 	}
 
 	if err := cli.Cmd(flag.Args()...); err != nil {
@@ -151,6 +239,18 @@ func main() {
 		}
 		log.Fatal(err)
 	}
+}
+
+func promptUnknownKey(key libtrust.PublicKey, host string) bool {
+	fmt.Printf("The authenticity of host %q can't be established.\nRemote key ID %s\n", host, key.KeyID())
+	fmt.Printf("Are you sure you want to continue connecting (yes/no)? ")
+	reader := bufio.NewReader(os.Stdin)
+	line, _, err := reader.ReadLine()
+	if err != nil {
+		log.Fatalf("Error reading input: %s", err)
+	}
+	input := strings.TrimSpace(strings.ToLower(string(line)))
+	return input == "yes" || input == "y"
 }
 
 func showVersion() {
