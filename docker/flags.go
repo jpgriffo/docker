7d9062787bb21228dc4f33ec17e8a18fc760338f
Derek McGowan <derek@mcgstyle.net>
2014-11-13 16:09:59 -0800

    Make the default authentication type 'none' to match existing behavior
    Signed-off-by: Derek McGowan <derek@mcgstyle.net> (github: dmcgowan)

--- docker/flags.go
+++ docker/flags.go
@@ -44,7 +44,7 @@ var (
 	flSocketGroup = flag.String([]string{"G", "-group"}, "docker", "Group to assign the unix socket specified by -H when running in daemon mode\nuse '' (the empty string) to disable setting of a group")
 	flLogLevel    = flag.String([]string{"l", "-log-level"}, "info", "Set the logging level")
 	flEnableCors  = flag.Bool([]string{"#api-enable-cors", "-api-enable-cors"}, false, "Enable CORS headers in the remote API")
-	flAuth        = flag.String([]string{"-auth"}, "identity", "Method used to authenticate the connection between client and daemon. Possible methods: identity, cert, none")
+	flAuth        = flag.String([]string{"-auth"}, "none", "Method used to authenticate the connection between client and daemon. Possible methods: identity, cert, none")
 	flAuthCa      = flag.String([]string{"-auth-ca"}, "", "Trust only remotes providing a certificate signed by the CA given here")
 	flAuthCert    = flag.String([]string{"-auth-cert"}, "", "Path to TLS certificate file")
 	flAuthKey     = flag.String([]string{"-auth-key"}, "", "Path to TLS key file")
