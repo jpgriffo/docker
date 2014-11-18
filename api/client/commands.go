18dd3d993c244cb028934c4a7e00265309bc7ce5
Ben Firshman <ben@firshman.co.uk>
2014-11-18 17:21:48 +0000

    Add Azure driver for hosts

--- api/client/commands.go
+++ api/client/commands.go
@@ -30,6 +30,7 @@ import (
 	"github.com/docker/docker/graph"
 	"github.com/docker/docker/hosts"
 	"github.com/docker/docker/hosts/drivers"
+	_ "github.com/docker/docker/hosts/drivers/azure"
 	_ "github.com/docker/docker/hosts/drivers/digitalocean"
 	_ "github.com/docker/docker/hosts/drivers/none"
 	_ "github.com/docker/docker/hosts/drivers/virtualbox"
